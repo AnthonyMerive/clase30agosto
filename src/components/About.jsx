ac1881469b5cd231d96cc8077c5f288dff951bb8
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-08-30 12:08:21 -0500

    finish config

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import About2 from './About2'
+
+export const About = () => {
+    return (
+            <>
+                <h1>About</h1>
+                <About2/>
+            </>
+                
+    )
+}
---
+++ src/components/About2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About2 = () => {
+    return (
+            <div>
+                <h1>About 2</h1>
+            </div>
+    )
+}
+
+export default About2;
